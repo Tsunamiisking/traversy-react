17522594f626e7b7e52fd490b260a24b34a1fcca
Allen Douglas <allenissabigboi@gmail.com>
2024-09-06 11:40:51 -0700

    Completed HomePage component

--- traversy-react/src/layouts/MainLayout.jsx
+++ traversy-react/src/layouts/MainLayout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Navbar />
+    <Outlet />
+    </>
   )
 }
 
