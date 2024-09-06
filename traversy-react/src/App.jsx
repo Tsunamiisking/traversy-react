210ffcb6afefed7ed7c033e58bb7604ff51c1cc0
Allen Douglas <allenissabigboi@gmail.com>
2024-09-06 11:07:05 -0700

    Created boiler plate for router in app.jsx component and modiefied the structure of the program

--- traversy-react/src/App.jsx
+++ traversy-react/src/App.jsx
@@ -1,28 +1,19 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from './components/HomeCards'
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from "./pages/HomePage";
 
+
+const router = createBrowserRouter (
+ createRoutesFromElements (<Route index element={<HomePage/>}/>)
+)
 const App = () => {
   return (
-    <>
-      {/* Nav bar */}
-      <Navbar />
-    
-      {/* <!-- Hero --> */}
-      <Hero />
-
-      {/* <!-- Developers and Employers --> */}
-      <HomeCards />
-
-      {/* <!-- Browse Jobs --> */}
-      <JobListings />
-
-      {/* View All jobs */}
-      <ViewAllJobs />
-    </>
+    <RouterProvider router={router}/>
   );
 };
 
