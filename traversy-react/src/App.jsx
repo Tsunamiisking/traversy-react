a9404f803da7d92034b7b12e08b446fec8ef7066
Allen Douglas <allenissabigboi@gmail.com>
2024-09-06 11:12:56 -0700

    Added layout to App.js component

--- traversy-react/src/App.jsx
+++ traversy-react/src/App.jsx
@@ -6,10 +6,15 @@ import {
 } from "react-router-dom";
 import React from "react";
 import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
 
 
 const router = createBrowserRouter (
- createRoutesFromElements (<Route index element={<HomePage/>}/>)
+ createRoutesFromElements (
+  <Route path="/" element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+  </Route>
+ )
 )
 const App = () => {
   return (
