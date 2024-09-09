f383b8e3bcfea63c8d22972f19777bc20d228cb7
Allen Douglas <allenissabigboi@gmail.com>
2024-09-09 15:01:38 -0700

    Added loading feature and installed spinners

--- traversy-react/src/components/JobListings.jsx
+++ traversy-react/src/components/JobListings.jsx
@@ -2,39 +2,44 @@ import React from "react";
 import JobListing from "./JobListing";
 import { useState, useEffect } from "react";
 
-const JobListings = ({isHome = false}) => {
+const JobListings = ({ isHome = false }) => {
   // Checking Array
   // console.log(Jobs)
 
-  const [Jobs, setJobs] = useState([])
-  const [loading, setLoading] = useState(true)
-
+  const [Jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchJobs = async () => {
-      try{
-        const res = await fetch('http://localhost:8000/Jobs')
-        const data = await res.json()
-        setJobs(data)
+      try {
+        const res = await fetch("http://localhost:8000/Jobs");
+        const data = await res.json();
+        setJobs(data);
       } catch (error) {
-        conosle.log('Error: ', error)
+        conosle.log("Error: ", error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
     fetchJobs();
-  }, [])
+  }, []);
   // const Jobs = isHome ? Jobs : Jobs.slice(0,3)
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          {isHome ? 'All Jobs' : 'Recent Jobs'}
+          {isHome ? "All Jobs" : "Recent Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {Jobs.map((job) => {
-            return <JobListing key={job.id} job={job} />;
-          })}
+          {loading ? (
+            <h2> Loading</h2>
+          ) : (
+            <>
+              {Jobs.map((job) => {
+                return <JobListing key={job.id} job={job} />;
+              })}
+            </>
+          )}
         </div>
       </div>
     </section>
