9f21ae35b9114e9234dd324fc9e47308baf31ddb
Allen Douglas <allenissabigboi@gmail.com>
2024-09-09 14:33:13 -0700

    Configured Joblisting to fetch data

--- traversy-react/src/components/JobListings.jsx
+++ traversy-react/src/components/JobListings.jsx
@@ -6,7 +6,25 @@ const JobListings = ({isHome = false}) => {
   // Checking Array
   // console.log(Jobs)
 
-  const availableJobListings = isHome ? Jobs : Jobs.slice(0,3)
+  const [Jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try{
+        const res = await fetch('http://localhost:8000/Jobs')
+        const data = await res.json()
+        setJobs(data)
+      } catch (error) {
+        conosle.log('Error: ', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchJobs();
+  }, [])
+  // const Jobs = isHome ? Jobs : Jobs.slice(0,3)
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -14,7 +32,7 @@ const JobListings = ({isHome = false}) => {
           {isHome ? 'All Jobs' : 'Recent Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {availableJobListings.map((job) => {
+          {Jobs.map((job) => {
             return <JobListing key={job.id} job={job} />;
           })}
         </div>
