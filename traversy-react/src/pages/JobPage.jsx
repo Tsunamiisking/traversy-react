f5820dc8325629d4fbd158c887e341ff3efac732
Allen Douglas <allenissabigboi@gmail.com>
2024-09-11 15:20:51 -0700

    Added useState logic for jobPage

--- traversy-react/src/pages/JobPage.jsx
+++ traversy-react/src/pages/JobPage.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const JobPage = () => {
+    const {id} = useParams();
+    const [job, setJob] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+              const apiUrl = isHome ? '/api/Jobs' : '/api/Jobs?_limit=3'
+              const res = await fetch(apiUrl);
+              const data = await res.json();
+              setJobs(data);
+            } catch (error) {
+              conosle.log("Error: ", error);
+            } finally {
+              setLoading(false);
+            }
+          };
+          fetchJobs();
+        }, []);
   return (
     <div>
       
