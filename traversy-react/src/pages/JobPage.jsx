142b277cccfa3ac569688de21fdb75bca1d1f492
Allen Douglas <allenissabigboi@gmail.com>
2024-09-15 15:54:21 -0700

    Configured Job page

--- traversy-react/src/pages/JobPage.jsx
+++ traversy-react/src/pages/JobPage.jsx
@@ -4,29 +4,29 @@ import Spinner from "../components/Spinner";
 
 const JobPage = () => {
   const { id } = useParams();
-  const [Job, setJob] = useState([]);
+  const [job, setJob] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        const apiUrl = `api/Jobs/${id}`;  
-        const res = await fetch(apiUrl);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.log("Error: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchJobs = async () => {
+  //     try {
+  //       const apiUrl = `api/Jobs/${id}`;  
+  //       const res = await fetch(apiUrl);
+  //       const data = await res.json();
+  //       setJob(data);
+  //     } catch (error) {
+  //       console.log("Error: ", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
     fetchJobs();
   }, [id]);
 
   return loading ? (
     <Spinner />
   ) : (
-    <h1>{Job.title}</h1>
+    <h1>{job.title}</h1>
   );
 };
 
