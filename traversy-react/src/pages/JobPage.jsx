df87bdc58561717514918054936f79fb5a9de2a9
Allen Douglas <allenissabigboi@gmail.com>
2024-10-02 15:19:20 -0700

    Added Jobpage loader to App.jsx

--- traversy-react/src/pages/JobPage.jsx
+++ traversy-react/src/pages/JobPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLoaderData } from "react-router-dom";
 import Spinner from "../components/Spinner";
 
 const JobPage = () => {
