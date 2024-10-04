ebed12674fc9e5279d1f1f0ba7fc19b3bef4b244
Allen Douglas <allenissabigboi@gmail.com>
2024-10-03 20:48:14 -0700

    Added edit job page

--- traversy-react/src/App.jsx
+++ traversy-react/src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
 import JobPage, { jobLoader } from "./pages/JobPage";
 import AddJob from "./pages/AddJob";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
   // Add New Job
@@ -42,6 +43,7 @@ const App = () => {
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJob addJobSubmit={addJob} />} />
         <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobLoader} />
+        <Route path="/edit-job/:id" element={<EditJobPage />} loader={jobLoader} />
         <Route path="*" element={<NotFound />} />
       </Route>
     )
